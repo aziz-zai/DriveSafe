5f2e7d1f2d85ce9828675a2c39a037b8ceab7b14
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-09-08 16:04:55 +0200

    env variables

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
-import { ExpoConfig, ConfigContext } from "@expo/config";
+import { ConfigContext, ExpoConfig } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_cmVsaWV2ZWQtY2ljYWRhLTEuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
